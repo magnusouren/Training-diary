90bd90c0bfab0076e8f2e71f9bfa3992f3b4439a
Magnus Ouren <magnut0203@gmail.com>
2022-03-06 22:03:29 +0100

    Add interface

---
+++ src/main/java/trainingDiary/Workout.java
@@ -0,0 +1,15 @@
+package trainingDiary;
+
+import java.util.Date;
+
+public interface Workout {
+
+    public Date getDate();
+
+    public int getDuration();
+
+    public int getRating();
+
+    public String getContent();
+
+}
