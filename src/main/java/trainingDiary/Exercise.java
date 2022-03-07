10e41a0fb1d3e141ae2cb82d26da0bc4bac994b5
Magnus Ouren <magnut0203@gmail.com>
2022-03-07 12:13:00 +0100

    Add Exercise.java with constructors

---
+++ src/main/java/trainingDiary/Exercise.java
@@ -0,0 +1,28 @@
+package trainingDiary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercise {
+
+    String name;
+    int sets;
+    List<Integer> reps = new ArrayList<>();
+
+    public Exercise(String name) {
+
+        this.name = name;
+
+    }
+
+    void setSets(int sets) {
+        this.sets = sets;
+    }
+
+    void addRep(int rep) {
+        if (reps.size() < sets)
+            reps.add(rep);
+        else
+            throw new IllegalArgumentException("To many sets added");
+    }
+}
