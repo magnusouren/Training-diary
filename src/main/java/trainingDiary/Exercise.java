36af0705220330e0b95033335f5041901dcca457
Magnus Ouren <magnut0203@gmail.com>
2022-03-07 20:00:48 +0100

    Add tostring

--- src/main/java/trainingDiary/Exercise.java
+++ src/main/java/trainingDiary/Exercise.java
@@ -1,6 +1,7 @@
 package trainingDiary;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Exercise {
@@ -39,6 +40,21 @@ public class Exercise {
 
     public List<Integer> getReps() {
         return reps;
+    }
+
+    @Override
+    public String toString() {
+        String res = name;
+
+        if (new HashSet<Integer>(reps).size() == 1) {
+            res += reps.size() + " x " + reps.get(0);
+        } else {
+            for (int i = 0; i < reps.size(); i++) {
+                res += ", " + reps.get(i);
+            }
+        }
+
+        return res;
     }
 
 }
