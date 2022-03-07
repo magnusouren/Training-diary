e40cd2233e86bb2563b3d6dc3b149a9b95e85879
Magnus Ouren <magnut0203@gmail.com>
2022-03-07 20:48:30 +0100

    Remove main

--- src/main/java/trainingDiary/Diary.java
+++ src/main/java/trainingDiary/Diary.java
@@ -20,18 +20,4 @@ public class Diary {
         }
         return res;
     }
-
-    public static void main(String[] args) {
-        Date now = new Date();
-        Workout run1 = new Run(now, 60, 10, '4', "Test", 150, 180);
-        Workout run2 = new Run(now, 80, 10, '4', "Test", 150, 180);
-        Workout styrke = new Strength(now, 50, '3', "Braaa");
-
-        Diary diary = new Diary();
-        diary.addWorkout(run1);
-        diary.addWorkout(run2);
-        diary.addWorkout(styrke);
-
-        System.out.println(diary);
-    }
 }
