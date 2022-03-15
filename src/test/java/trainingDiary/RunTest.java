fdf98d28b680016c7da94417da29e02b0a476a83
Magnus Ouren <magnut0203@gmail.com>
2022-03-15 13:43:24 +0100

    Add test on duration and date

--- src/test/java/trainingDiary/RunTest.java
+++ src/test/java/trainingDiary/RunTest.java
@@ -15,20 +15,12 @@ public class RunTest {
 
     @BeforeEach
     public void setup() {
-        run = new Run(LocalDateTime.now().minusSeconds(1), 60, 5000, '3', "Test", 150, 200);
+        run = new Run(LocalDateTime.now(), 60, 5000, '3', "Test", 150, 200);
     }
 
     @Test
     public void testConstructor() {
-        LocalDateTime now = LocalDateTime.now().minusSeconds(1);
 
-        assertEquals(now.getYear(), run.getDate().getYear(), "Workout should have same year-value as today");
-        assertEquals(now.getMonth(), run.getDate().getMonth(), "Workout should have same month-value as today");
-        assertEquals(now.getDayOfMonth(), run.getDate().getDayOfMonth(), "Workout should have same day-value as today");
-        assertEquals(now.getHour(), run.getDate().getHour(), "Workout should have same hour-value as today");
-        assertEquals(now.getMinute(), run.getDate().getMinute(), "Workout should have same minute-value as today");
-
-        assertEquals(60, run.getDuration(), "Duration should be 60 when sat to 60");
         assertEquals(5000, run.getDistance());
         assertEquals('3', run.getRating(), "Rating should be '3'");
         assertEquals("Test", run.getContent());
@@ -37,4 +29,37 @@ public class RunTest {
 
     }
 
+    @Test
+    public void testDate() {
+
+        // Sjekker satt dato opp mot nå, nøyaktig nok i denne sammenhengen
+        LocalDateTime date = LocalDateTime.now();
+        assertEquals(date.getYear(), run.getDate().getYear(), "Workout should have same year-value as today");
+        assertEquals(date.getMonth(), run.getDate().getMonth(), "Workout should have same month-value as today");
+        assertEquals(date.getDayOfMonth(), run.getDate().getDayOfMonth(),
+                "Workout should have same day-value as today");
+        assertEquals(date.getHour(), run.getDate().getHour(), "Workout should have same hour-value as today");
+        assertEquals(date.getMinute(), run.getDate().getMinute(), "Workout should have same minute-value as today");
+
+        run = new Run(LocalDateTime.of(2021, 3, 2, 12, 00), 60, 5000, '3', "Test", 150, 200);
+
+        // Sjekker datoer opp mot hverandre
+        date = LocalDateTime.of(2021, 3, 2, 12, 00);
+        assertEquals(date.getYear(), run.getDate().getYear(), "Workout should have same year-value as date set");
+        assertEquals(date.getMonth(), run.getDate().getMonth(), "Workout should have same month-value as date set");
+        assertEquals(date.getDayOfMonth(), run.getDate().getDayOfMonth(),
+                "Workout should have same day-value as today");
+        assertEquals(date.getHour(), run.getDate().getHour(), "Workout should have same hour-value as date set");
+        assertEquals(date.getMinute(), run.getDate().getMinute(), "Workout should have same minute-value as date set");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            run = new Run(LocalDateTime.of(2023, 3, 2, 12, 00), 60, 5000, '3', "Test", 150, 200);
+        }, "Datoer frem i tid skal ikke kunne settes?");
+    }
+
+    @Test
+    public void testDuration() {
+        assertEquals(60, run.getDuration(), "Duration should be 60 when sat to 60");
+
+    }
 }
