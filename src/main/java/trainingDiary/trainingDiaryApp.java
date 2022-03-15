94b7e6cba5d918227656d0595d44e4b5bb397f5a
Magnus Ouren <magnut0203@gmail.com>
2022-03-15 13:42:21 +0100

    Add launch for fxml file

--- src/main/java/trainingDiary/trainingDiaryApp.java
+++ src/main/java/trainingDiary/trainingDiaryApp.java
@@ -1,5 +1,21 @@
 package trainingDiary;
 
-public class trainingDiaryApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class trainingDiaryApp extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("Training Diary");
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("App.fxml"))));
+        primaryStage.show();
+    }
 }
