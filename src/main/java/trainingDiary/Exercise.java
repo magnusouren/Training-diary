be9ffa070368380238438fc5cfdab68b489e03a8
Magnus Ouren <magnut0203@gmail.com>
2022-03-19 14:53:36 +0100

    Remove import

--- src/main/java/trainingDiary/Exercise.java
+++ src/main/java/trainingDiary/Exercise.java
@@ -3,7 +3,6 @@ package trainingDiary;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Exercise {
