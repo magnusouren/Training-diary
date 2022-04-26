088657c83109535617c4006c2a29db4240c1473d
Magnus Ouren <magnut0203@gmail.com>
2022-04-26 17:50:55 +0200

    Remove import

--- src/main/java/trainingDiary/validation/Commons.java
+++ src/main/java/trainingDiary/validation/Commons.java
@@ -4,7 +4,6 @@ import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.regex.PatternSyntaxException;
 
 import javafx.css.PseudoClass;
 import javafx.scene.Node;
