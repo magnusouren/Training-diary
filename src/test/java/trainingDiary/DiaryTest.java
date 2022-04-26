7198f1e86b9fe3e342117769be6690c1809a80bb
Magnus Ouren <magnut0203@gmail.com>
2022-04-26 22:41:08 +0200

    Add tests on workoutspermonth

--- src/test/java/trainingDiary/DiaryTest.java
+++ src/test/java/trainingDiary/DiaryTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -48,7 +51,7 @@ public class DiaryTest {
             diary.addWorkout(run);
         }, "Workout cannot be added if workout already exists in diary");
 
-        strength.setDate(LocalDateTime.now().minusDays(1));
+        strength.setDate(run.getDate());
 
         assertThrows(IllegalArgumentException.class, () -> {
             diary.addWorkout(strength);
@@ -58,6 +61,37 @@ public class DiaryTest {
             diary.addWorkout(null);
         }, "Null should not be allowed to be added to diary");
 
+    }
+
+    @Test
+    public void testGetWorkoutsPerMonth() {
+
+        List<LocalDateTime> dates = new ArrayList<>();
+        dates.add(LocalDateTime.of(2022, 01, 01, 12, 00));
+        dates.add(LocalDateTime.of(2022, 01, 02, 12, 00));
+        dates.add(LocalDateTime.of(2022, 01, 03, 12, 00));
+        dates.add(LocalDateTime.of(2022, 01, 04, 12, 00));
+        dates.add(LocalDateTime.of(2022, 01, 05, 12, 00));
+        dates.add(LocalDateTime.of(2022, 02, 01, 12, 00));
+        dates.add(LocalDateTime.of(2022, 02, 02, 12, 00));
+        dates.add(LocalDateTime.of(2022, 02, 03, 12, 00));
+        dates.add(LocalDateTime.of(2022, 02, 04, 12, 00));
+
+        Iterator<LocalDateTime> ItDates = dates.iterator();
+
+        while (ItDates.hasNext()) {
+            diary.addWorkout(new Strength(ItDates.next(), 60, '5', "test"));
+        }
+
+        assertEquals(0, diary.getWorkoutsPerMonth(03, 2022).size(), "Month shouldn't contain workouts");
+
+        for (IWorkout workout : diary.getWorkoutsPerMonth(01, 2022)) {
+            assertEquals(01, workout.getDate().getMonthValue(), "Month value should be equal");
+        }
+
+        for (IWorkout workout : diary.getWorkoutsPerMonth(02, 2022)) {
+            assertEquals(02, workout.getDate().getMonthValue(), "Month value should be equal");
+        }
     }
 
 }
